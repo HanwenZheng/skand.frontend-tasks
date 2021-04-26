4c0f017149e19aae39f02decf6f0867299f9c032
Alex Zheng <alex.zhenghw@gmail.com>
2021-04-26 19:43:37 +1000

    Feature: redirect on 404 home/id while logged in

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -7,7 +7,7 @@ import Button from "@material-ui/core/Button";
 const User = ({ match, users }) => {
   const id = match.params.id;
   const user = users.find((user) => user.id === id);
-  // if (!user) return <Redirect to="/home" />;
+  if (users.length && !user) return <Redirect to="/home" />;
 
   return (
     <Fragment>
