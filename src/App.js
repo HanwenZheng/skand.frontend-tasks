cc201af5f48cfdbe8efd76169301e2e3beff0c41
Alex Zheng <alex.zhenghw@gmail.com>
2021-05-25 14:51:18 +1000

    install packages and setup

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import React from "react";
 import axios from "axios";
+import { Route, Switch } from "react-router-dom";
+
+import Login from "./Components/Login";
 
 function App() {
-  const proxy = "/api/v2";
-  const config = {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-  const body = JSON.stringify({ email: "test@skand.io", password: "password" });
-
-  const getData = async () => {
-    const res = await axios.post(proxy + "/users/tokens", body, config);
-    return res;
-  };
-
-  try {
-    getData().then((data) => {
-      console.log(data);
-    });
-  } catch (err) {
-    console.error(err.message);
-  }
-
-  return <div className="App">ok</div>;
+  return (
+    <div className="App">
+      <Route exact path="/" component={Login} />
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./mockServer";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./Redux/Store/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
